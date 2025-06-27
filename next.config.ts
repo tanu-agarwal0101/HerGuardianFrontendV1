fb626a5e005ecaed961d81f816d0dd1c15bdaff6
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-06-27 16:17:45 +0530

    feat: Basic setup

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,50 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  swSrc: "public/service-worker.js",
+  // runtimeCaching: [
+  //   {
+  //     urlPattern: /^https:\/\/example\.com\/api\/.*$/,
+  //     handler: 'NetworkFirst',
+  //     options: {
+  //       cacheName: 'api-cache',
+  //       expiration: {
+  //         maxEntries: 50,
+  //         maxAgeSeconds: 60 * 60 * 24, // 1 day
+  //       },
+  //     },
+  //   },
+  //   {
+  //     urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|ico)$/,
+  //     handler: 'CacheFirst',
+  //     options: {
+  //       cacheName: 'image-cache',
+  //       expiration: {
+  //         maxEntries: 100,
+  //         maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+  //       },
+  //     },
+  //   },
+  //   {
+  //     urlPattern: /\.(?:js|css|woff2|woff|ttf|eot)$/,
+  //     handler: 'StaleWhileRevalidate',
+  //     options: {
+  //       cacheName: 'static-resources',
+  //       expiration: {
+  //         maxEntries: 100,
+  //         maxAgeSeconds: 60 * 60 * 24 * 30, // 1 month
+  //       },
+  //     },
+  //   },
+  // ],
+});
+
+module.exports = withPWA({
+  ...nextConfig,
+  reactStrictMode: true,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: ["bg-blue-500", "bg-pink-500", "bg-green-500"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
